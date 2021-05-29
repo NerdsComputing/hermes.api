fd840a1680aace9c9b1cc6db55629f4e86ad6d09
Radu B <radu.buciuceanu@gmail.com>
2021-05-29 19:08:29 +0300

    [chaos] Version arguement for dockerfile

--- scripts/docker.build.release.py
+++ scripts/docker.build.release.py
@@ -7,6 +7,10 @@ parser.add_argument('--tag',
                     default='nerdscomputing/hermes.api:latest',
                     help='The tag for the new created docker image.')
 
+parser.add_argument('--version',
+                    default='2021.0.0.0',
+                    help='The semantic version of the api.')
+
 args = parser.parse_args()
 
 system(f'docker build -t {args.tag} -f dockerfile.release .')
