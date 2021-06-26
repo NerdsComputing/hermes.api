112cb81dc26ff40bd1ae6a2bebda06dc3b7f84e2
Codrut <constantinescu321@gmail.com>
2021-06-27 01:25:45 +0300

    [HER-32]Follow-up-Changing configurations

--- src/Presentation/Program.cs
+++ src/Presentation/Program.cs
@@ -1,14 +1,20 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace Presentation
 {
     public static class Program
     {
+        
         public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
 
         private static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostContext, config) =>
+                {
+                    config.AddJsonFile("settings.default.json", optional: true, reloadOnChange: true);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>  webBuilder.UseStartup<Startup>()); 
     }
 }
