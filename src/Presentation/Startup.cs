2d4802db5f9b46e6a097fb63302356fe77bd157f
bianca <bia.topircean@gmail.com>
2021-06-27 22:34:49 +0300

    [HER-28] Setup Data Project https://nerds-interns.atlassian.net/browse/HER-28?atlOrigin=eyJpIjoiNGI2MmQ3OGQ2ZjFkNDAyNGI4Mzc5ZjYzMTBjYzk1MTYiLCJwIjoiaiJ9

--- src/Presentation/Startup.cs
+++ src/Presentation/Startup.cs
@@ -1,21 +1,38 @@
 using System;
+using Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace Presentation
 {
+    
     public class Startup
     {
+        
+        public string ConnectionString { get; set; }
         private readonly IConfiguration _config;
         
-        public Startup(IConfiguration config) =>_config = config;
-        
-        public static void ConfigureServices(IServiceCollection services)
+        public Startup(IConfiguration configuration)
         {
+            _config = configuration;
+            Configuration = configuration;
+            ConnectionString = Configuration.GetConnectionString("DefaultConnection");
+        }
+        
+       
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            ConnectionString = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<DataContext>(
+                options => options.UseSqlServer(ConnectionString));
             services.AddControllers();
         }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
