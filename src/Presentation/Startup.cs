3001ce80193f040b81a22b43c7b18216e2e24701
TBiannca <58395222+TBiannca@users.noreply.github.com>
2021-06-28 15:22:47 +0300

    [HER-28] Setup data project (#9)
    * [HER-28] Setup Data Project
    https://nerds-interns.atlassian.net/browse/HER-28?atlOrigin=eyJpIjoiNGI2MmQ3OGQ2ZjFkNDAyNGI4Mzc5ZjYzMTBjYzk1MTYiLCJwIjoiaiJ9
    
    * [HER-28]Follow up - Changed from Sql to Mysql
    
    * [HER-28]Import missing library
    
    * [HER-28]Follow-up
    
    * [HER-28]Follow-up
    
    * [HER-28]Change "caca" to connectionString

--- src/Presentation/Startup.cs
+++ src/Presentation/Startup.cs
@@ -1,21 +1,26 @@
 using System;
+using Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace Presentation
 {
+    
     public class Startup
     {
         private readonly IConfiguration _config;
         
-        public Startup(IConfiguration config) =>_config = config;
-        
-        public static void ConfigureServices(IServiceCollection services)
+        public Startup(IConfiguration configuration, IConfiguration config) => _config = config;
+
+        public void ConfigureServices(IServiceCollection services)
         {
+            var connectionString = _config.GetSection("Database").GetValue<string>("ConnectionString");
+            services.AddDbContext<Context>(options =>
+                 options.UseMySQL(connectionString));
             services.AddControllers();
         }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
