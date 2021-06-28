e9e72229b0b0395de6c787b243ca4cbb376aed72
bianca <bia.topircean@gmail.com>
2021-06-28 13:48:47 +0300

    [HER-28]Import missing library

--- src/Presentation/Startup.cs
+++ src/Presentation/Startup.cs
@@ -3,6 +3,7 @@ using Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
