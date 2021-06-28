def21241b317a47a0fa4b3d4ae3220ecf365d329
Codrut <constantinescu321@gmail.com>
2021-06-28 10:31:07 +0300

    [HER-32]Follow-up-Configure to read from env

--- src/Presentation/Program.cs
+++ src/Presentation/Program.cs
@@ -6,14 +6,14 @@ namespace Presentation
 {
     public static class Program
     {
-        
         public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
 
         private static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureAppConfiguration((hostContext, config) =>
                 {
-                    config.AddJsonFile("settings.default.json", optional: true, reloadOnChange: true);
+                    config.AddJsonFile("settings/settings.default.json", optional: true, reloadOnChange: true);
+                    config.AddEnvironmentVariables("HERMES_API_");
                 })
                 .ConfigureWebHostDefaults(webBuilder =>  webBuilder.UseStartup<Startup>()); 
     }
