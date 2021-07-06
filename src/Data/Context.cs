94224fab9215849ec835e011d07ea0eb56664e80
TBiannca <58395222+TBiannca@users.noreply.github.com>
2021-07-06 18:23:56 +0300

    [HER-41] Configure database connection (#12)

--- src/Data/Context.cs
+++ src/Data/Context.cs
@@ -2,8 +2,12 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Data
 {
+
     public class Context : DbContext
     {
+        public DbSet<EDetections> Detections { get; set; }
         public Context(DbContextOptions<Context> options) : base(options) { }
+
     }
+
 }
---
+++ src/Data/EDetections.cs
@@ -0,0 +1,8 @@
+namespace Data
+{
+    public class EDetections
+    {
+        public int Id{ get; set; }
+        public string Name { get; set; }
+    }
+}
