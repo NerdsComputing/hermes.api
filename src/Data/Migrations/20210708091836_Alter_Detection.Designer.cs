afcf59c1d6d2a84d854411b8cb638698b6ffb41d
bianca <bia.topircean@gmail.com>
2021-07-08 12:20:43 +0300

    [HER-57]Rename EDetections

--- src/Data/Migrations/20210708091836_Alter_Detection.Designer.cs
+++ src/Data/Migrations/20210708091836_Alter_Detection.Designer.cs
@@ -2,14 +2,16 @@
 using Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Data.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20210708091836_Alter_Detection")]
+    partial class Alter_Detection
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
