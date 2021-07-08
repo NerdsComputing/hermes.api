c8eaa01227f29d737b394419c6df118540ea78d2
bianca <bia.topircean@gmail.com>
2021-07-08 14:01:37 +0300

    [HER-46] Migrate entity structure

--- src/Data/EDetection.cs
+++ src/Data/EDetection.cs
@@ -1,9 +1,16 @@
 namespace Data
 {
+    using System;
+    using System.ComponentModel.DataAnnotations.Schema;
+
     public class EDetection
     {
         public int Id { get; set; }
 
         public int Score { get; set; }
+
+        public string Class { get; set; }
+
+        public DateTime Timestamp { get; set; }
     }
 }
