29ff72317067d4b34ce00c245050663c26213ab6
radu <39530866+RaduBuciuceanu@users.noreply.github.com>
2021-07-08 12:44:30 +0300

    [HER-58] Configure informational version (#19)
    * [HER-58] Change release dockerfile
    
    * [HER-58] Change docker build script
    
    * [HER-58] Configure informational version
    
    * [HER-58] Change status controller

--- src/Presentation/Status/Controller.cs
+++ src/Presentation/Status/Controller.cs
@@ -1,5 +1,6 @@
 namespace Presentation.Status
 {
+    using System.Diagnostics;
     using System.Reflection;
     using Microsoft.AspNetCore.Mvc;
 
@@ -7,6 +8,16 @@ namespace Presentation.Status
     public class Controller : Microsoft.AspNetCore.Mvc.Controller
     {
         [HttpGet("version")]
-        public IActionResult Version() => Ok(Assembly.GetExecutingAssembly().FullName);
+        public IActionResult Version()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var version = FileVersionInfo.GetVersionInfo(assembly.Location);
+
+            return Ok(new
+            {
+                @short = version.FileVersion,
+                @long = version.ProductVersion,
+            });
+        }
     }
 }
