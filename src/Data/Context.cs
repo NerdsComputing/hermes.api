71715532fd9052512f6ecbef06cdabd2653ad880
Radu B <radu.buciuceanu@gmail.com>
2021-07-08 07:48:30 +0300

    [HER-56] Fix data issues

--- src/Data/Context.cs
+++ src/Data/Context.cs
@@ -1,13 +1,13 @@
-using Microsoft.EntityFrameworkCore;
-
 namespace Data
 {
+    using Microsoft.EntityFrameworkCore;
 
     public class Context : DbContext
     {
         public DbSet<EDetections> Detections { get; set; }
-        public Context(DbContextOptions<Context> options) : base(options) { }
 
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+        }
     }
-
 }
--- src/Data/EDetections.cs
+++ src/Data/EDetections.cs
@@ -2,7 +2,7 @@ namespace Data
 {
     public class EDetections
     {
-        public int Id{ get; set; }
+        public int Id { get; set; }
 
         public int Score { get; set; }
     }
