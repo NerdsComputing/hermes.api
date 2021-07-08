5639b7c6506d76b12cea7445ec44fca7df15b9a2
TBiannca <58395222+TBiannca@users.noreply.github.com>
2021-07-08 17:26:34 +0300

    [HER-46] Migrate entity structure (#21)
    * [HER-46] Migrate entity structure
    
    * [HER-46] Remove the underscores
    
    * [HER-46] Resolve conflicts
    
    * [HER-46] Rename migration
    
    * [HER-46] Follow-up Make fields required

--- src/Data/EDetection.cs
+++ src/Data/EDetection.cs
@@ -1,9 +1,20 @@
 namespace Data
 {
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
     public class EDetection
     {
+        [Required]
         public int Id { get; set; }
 
+        [Required]
         public int Score { get; set; }
+
+        [Required]
+        public string Class { get; set; }
+
+        [Required]
+        public DateTime Timestamp { get; set; }
     }
 }
---
+++ src/Data/Migrations/20210708131624_AlterDetectionsAddFields.cs
@@ -0,0 +1,35 @@
+﻿namespace Data.Migrations
+{
+    using System;
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    public partial class AlterDetectionsAddFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Class",
+                table: "Detections",
+                type: "text",
+                nullable: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Timestamp",
+                table: "Detections",
+                type: "datetime",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Class",
+                table: "Detections");
+
+            migrationBuilder.DropColumn(
+                name: "Timestamp",
+                table: "Detections");
+        }
+    }
+}
