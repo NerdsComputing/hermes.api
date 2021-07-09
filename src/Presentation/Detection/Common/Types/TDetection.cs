3451ae6e173d4510a0de1d91ae670183e0265f24
casian <casian.platon@ulbsibiu.ro>
2021-07-09 15:14:24 +0300

    [HER-49]Add TDetection

---
+++ src/Presentation/Detection/Common/Types/TDetection.cs
@@ -0,0 +1,19 @@
+namespace Presentation.Detection.Common.Types
+{
+    using System.Diagnostics.CodeAnalysis;
+    using Business.Detection.Common.Models;
+    using global::GraphQL.Types;
+    using Style;
+
+    [SuppressMessage(Category.Default, Check.CA1724, Justification = Reason.Readability)]
+    public sealed class TDetection : ObjectGraphType<MDetection>
+    {
+        public TDetection()
+        {
+            Field(detection => detection.Id).Description("This is the ID");
+            Field(detection => detection.Score).Description("This is the Score");
+            Field(detection => detection.Timestamp).Description("This is the Timestamp");
+            Field(detection => detection.Class).Description("This is the Class");
+        }
+    }
+}
