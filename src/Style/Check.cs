0492eba8ae2896f07b3171b9b972ddf2ff54f8f9
TBiannca <58395222+TBiannca@users.noreply.github.com>
2021-07-09 17:57:05 +0300

    [HER-47] Migrate initial detections (#27)
    * [HER-47] Add more checks
    
    * [HER-47] Change Context
    
    * [HER-47] Add migration
    
    * [HER-47] Follow-up Add more data
    
    * [HER-47] Remove unused System
    
    * [HER-47] Change Down func in migration
    
    * [HER-47] Remove paranthesis

--- src/Style/Check.cs
+++ src/Style/Check.cs
@@ -4,6 +4,8 @@ namespace Style
     {
         public const string CA1707 = "CA1707";
         public const string CA1724 = "CA1724";
+        public const string SA1118 = "SA1118";
+        public const string CA1814 = "CA1814";
         public const string CA1052 = "CA1052";
     }
 }
