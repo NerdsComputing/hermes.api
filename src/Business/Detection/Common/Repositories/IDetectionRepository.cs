789c5d65646cb12fb1eb54572f33e9fac60c1194
andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-07-19 22:02:39 +0300

    [HER-71]Create dummy method in repository

--- src/Business/Detection/Common/Repositories/IDetectionRepository.cs
+++ src/Business/Detection/Common/Repositories/IDetectionRepository.cs
@@ -2,9 +2,12 @@ namespace Business.Detection.Common.Repositories
 {
     using System.Collections.Generic;
     using Business.Detection.Common.Models;
+    using Business.Detection.Creating.Models;
 
     public interface IDetectionRepository
     {
         public IEnumerable<MDetection> ByParameter();
+
+        public MDetection Create(MCreateDetection item);
     }
 }
