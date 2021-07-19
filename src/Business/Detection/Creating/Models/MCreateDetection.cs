ad3a5aa30a56c74cfef24515901405b69fc86f3d
andreea-dumitrascu <72592844+andreea-dumitrascu@users.noreply.github.com>
2021-07-19 14:07:01 +0300

    [HER-68] Create business input (#29)
    * [HER-68]Create MCreateDetection model in business
    
    * [HER-68]Follow-up Move the MCreateDetection

---
+++ src/Business/Detection/Creating/Models/MCreateDetection.cs
@@ -0,0 +1,13 @@
+namespace Business.Detection.Creating.Models
+{
+    using System;
+
+    public class MCreateDetection
+    {
+        public int Score { get; set; }
+
+        public string Class { get; set; }
+
+        public DateTime Timestamp { get; set; }
+    }
+}
