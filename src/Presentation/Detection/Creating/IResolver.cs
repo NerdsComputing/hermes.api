592971c8ae76f25d098fc86ea669f9f424048262
casian <casian.platon@ulbsibiu.ro>
2021-07-19 12:44:39 +0300

    [HER-63]Remove unused imports

--- src/Presentation/Detection/Creating/IResolver.cs
+++ src/Presentation/Detection/Creating/IResolver.cs
@@ -1,7 +1,6 @@
 namespace Presentation.Detection.Creating
 {
     using System.Collections.Generic;
-    using Business;
     using Business.Detection.Common.Models;
     using Business.Detection.Fetching.Commands;
     using global::GraphQL;
