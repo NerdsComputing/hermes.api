7710238aabed8c2277a54f0505cf2328fa9438e9
andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-07-20 19:08:01 +0300

    [HER-71]Remove unused using

--- src/Presentation/Detection/Creating/IResolver.cs
+++ src/Presentation/Detection/Creating/IResolver.cs
@@ -2,9 +2,7 @@ namespace Presentation.Detection.Creating
 {
     using System.Collections.Generic;
     using Business;
-    using Business.Detection.Common.Models;
     using Business.Detection.Creating.Models;
-    using Business.Detection.Fetching.Commands;
     using global::GraphQL;
 
     public interface IResolver : ICommand<IResolveFieldContext<object>, IEnumerable<MCreateDetection>>
