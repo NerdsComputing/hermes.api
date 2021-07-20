41d627fbb012c387674f323ba3eac14d2fbd6f8a
andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-07-20 13:04:05 +0300

    [HER-71]Change using from ICreateDetection

--- src/Business/Detection/Creating/Commands/ICreateDetection.cs
+++ src/Business/Detection/Creating/Commands/ICreateDetection.cs
@@ -1,7 +1,6 @@
 namespace Business.Detection.Creating.Commands
 {
     using System.Collections.Generic;
-    using Business.Detection.Common.Commands;
     using Business.Detection.Common.Models;
     using Business.Detection.Creating.Models;
 
