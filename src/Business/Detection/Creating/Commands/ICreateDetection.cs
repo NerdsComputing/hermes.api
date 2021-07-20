0b11d993ec4718b21b0b0770df8935ac6b30f45f
andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-07-20 12:08:35 +0300

    [HER-71]Follow-up Change in interface to have a list

--- src/Business/Detection/Creating/Commands/ICreateDetection.cs
+++ src/Business/Detection/Creating/Commands/ICreateDetection.cs
@@ -1,10 +1,11 @@
 namespace Business.Detection.Creating.Commands
 {
+    using System.Collections.Generic;
     using Business.Detection.Common.Commands;
     using Business.Detection.Common.Models;
     using Business.Detection.Creating.Models;
 
-    public interface ICreateDetection : ICommand<MCreateDetection, MDetection>
+    public interface ICreateDetection : ICommand<IEnumerable<MCreateDetection>, IEnumerable<MDetection>>
     {
     }
 }
