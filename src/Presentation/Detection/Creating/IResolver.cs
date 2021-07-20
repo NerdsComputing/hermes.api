f65f45b859739e2e936074be34c41fd0527c5dd6
casian <casian.platon@ulbsibiu.ro>
2021-07-20 13:43:52 +0300

    [HER-70]Change to MCreateModel in IEnumerable

--- src/Presentation/Detection/Creating/IResolver.cs
+++ src/Presentation/Detection/Creating/IResolver.cs
@@ -1,11 +1,11 @@
 namespace Presentation.Detection.Creating
 {
     using System.Collections.Generic;
-    using Business.Detection.Common.Models;
+    using Business.Detection.Creating.Models;
     using Business.Detection.Fetching.Commands;
     using global::GraphQL;
 
-    public interface IResolver : ICommand<IResolveFieldContext<object>, IEnumerable<MDetection>>
+    public interface IResolver : ICommand<IResolveFieldContext<object>, IEnumerable<MCreateDetection>>
     {
     }
 }
