edcaf396af64f8a3871a37f391510f91bd899490
Codrut <constantinescu321@gmail.com>
2021-07-20 09:17:05 +0300

    [HER-53]Follow-up Create detection test

---
+++ FactoryDetectionTests/ConvertDetection.cs
@@ -0,0 +1,34 @@
+using Business.Detection.Common.Models;
+using Data;
+using Data.Detection;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace FactoryDetectionTests
+{
+    public class ConvertDetection
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var actualDetection = new MDetection {
+                Id = 1,
+                Class = "Class",
+                Score = 100
+            };
+            var testDetection = new EDetection {
+                Id = 1,
+                Class = "Class",
+                Score = 100
+            };
+            var outputDetection = DetectionFactory.MakeModel(testDetection);
+            
+            actualDetection.Should().BeEquivalentTo(outputDetection);
+        }
+    }
+}
