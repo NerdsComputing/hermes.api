e51b41d865965a44eeccef8f2f438bb45a3a755b
Codrut <constantinescu321@gmail.com>
2021-07-21 11:49:34 +0300

    [HER-72]Create detection test method

--- test/Data.UnitTests/Detection/DetectionFactoryTests.cs
+++ test/Data.UnitTests/Detection/DetectionFactoryTests.cs
@@ -2,6 +2,7 @@ namespace Data.UnitTests.Detection
 {
     using System;
     using Business.Detection.Common.Models;
+    using Business.Detection.Creating.Models;
     using Data.Detection;
     using NUnit.Framework;
     using Shouldly;
@@ -10,11 +11,13 @@ namespace Data.UnitTests.Detection
     {
         private MDetection _model;
         private EDetection _entity;
+        private MCreateDetection _modelCreate;
 
         [SetUp]
         public void Setup()
         {
             _model = CreateModel();
+            _modelCreate = CreateDetection();
             _entity = CreateEntity();
         }
 
@@ -26,20 +29,36 @@ namespace Data.UnitTests.Detection
             _model.ShouldBeEquivalentTo(outputDetection);
         }
 
+        [Test]
+        public void Factory_shouldConvert_MCreateDetection()
+        {
+            var outputDetection = DetectionFactory.MakeEntity(_modelCreate);
+            
+            _entity.ShouldBeEquivalentTo(outputDetection);
+        }
+
+        private static EDetection CreateEntity() => new()
         private static EDetection CreateEntity() => new ()
         {
-            Id = 1,
+            Id = 0,
             Class = "Class",
             Score = 100,
-            Timestamp = new DateTime(2021, 07, 20),
+            Timestamp = new DateTime(2021, 07, 20)
         };
 
-        private static MDetection CreateModel() => new ()
+        private static MCreateDetection CreateDetection() => new()
         {
-            Id = 1,
             Class = "Class",
             Score = 100,
-            Timestamp = new DateTime(2021, 07, 20),
+            Timestamp = new DateTime(2021, 07, 20)
+        };
+        
+        private static MDetection CreateModel() => new()
+        {
+            Id = 0,
+            Class = "Class",
+            Score = 100,
+            Timestamp = new DateTime(2021, 07, 20)
         };
     }
 }
