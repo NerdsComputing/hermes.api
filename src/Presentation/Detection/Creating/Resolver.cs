92618815479418734a272548a5df896b2901f700
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-07-22 22:56:15 +0300

    [HER-73]Resolver now returns a created detection

--- src/Presentation/Detection/Creating/Resolver.cs
+++ src/Presentation/Detection/Creating/Resolver.cs
@@ -1,16 +1,21 @@
 namespace Presentation.Detection.Creating
 {
-    using System;
     using System.Collections.Generic;
+    using Business.Detection.Common.Models;
+    using Business.Detection.Creating.Commands;
     using Business.Detection.Creating.Models;
     using global::GraphQL;
 
     public class Resolver : IResolver
     {
-        public IEnumerable<MCreateDetection> Execute(IResolveFieldContext<object> input) =>
-            new[]
-            {
-                new MCreateDetection() { Class = "Pet", Score = 60, Timestamp = DateTime.UtcNow },
-            };
+        private readonly ICreateDetection _createDetection;
+
+        public Resolver(ICreateDetection createDetection) => _createDetection = createDetection;
+
+        public IEnumerable<MDetection> Execute(IResolveFieldContext<object> input)
+        {
+            var detection = input.GetArgument<IEnumerable<MCreateDetection>>("input");
+            return _createDetection.Execute(detection);
+        }
     }
 }
