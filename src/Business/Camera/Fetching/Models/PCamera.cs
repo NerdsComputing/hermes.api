d11bb441532786a448c8bca57abb6aa00f8feec9
Codrut <constantinescu321@gmail.com>
2021-07-26 11:13:25 +0300

    [HER-86]Create PCamera model

---
+++ src/Business/Camera/Fetching/Models/PCamera.cs
@@ -0,0 +1,9 @@
+namespace Business.Camera.Fetching.Models
+{
+    using System.Collections.Generic;
+
+    public class PCamera
+    {
+        public List<string> Ids { get; set; } = new List<string>();
+    }
+}
