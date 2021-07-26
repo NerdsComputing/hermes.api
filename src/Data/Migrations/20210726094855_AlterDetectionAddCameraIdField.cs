42fbcb7553cd463c999b1620ee1922426e096af3
bianca <bia.topircean@gmail.com>
2021-07-26 12:50:28 +0300

    [HER-93] Create migration for adding CameraId

---
+++ src/Data/Migrations/20210726094855_AlterDetectionAddCameraIdField.cs
@@ -0,0 +1,23 @@
+﻿namespace Data.Migrations
+{
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    public partial class AlterDetectionAddCameraIdField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CameraId",
+                table: "Detections",
+                type: "text",
+                nullable: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CameraId",
+                table: "Detections");
+        }
+    }
+}
