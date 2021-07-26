638819817e36e78e92e866504cd63c897bb541d5
Codrut <constantinescu321@gmail.com>
2021-07-26 11:52:15 +0300

    [HER-86]Follow-up Add nullable to ids

--- src/Business/Camera/Fetching/Models/PCamera.cs
+++ src/Business/Camera/Fetching/Models/PCamera.cs
@@ -3,11 +3,9 @@ namespace Business.Camera.Fetching.Models
     using System.Collections.Generic;
     using Business.Pagination;
 
-    #nullable enable
-
     public class PCamera
     {
-        public List<string> Ids { get; set; }
+        public List<string>? Ids { get; set; }
 
         public PPagination Pagination { get; set; } = new PPagination();
     }
