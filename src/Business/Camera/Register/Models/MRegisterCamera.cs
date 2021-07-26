3ed448b0c3d91917b4ddefc11fb14a8ae03daac3
andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-07-26 11:04:09 +0300

    [HER-81]Create MRegisterCamera business input

---
+++ src/Business/Camera/Register/Models/MRegisterCamera.cs
@@ -0,0 +1,11 @@
+namespace Business.Camera.Register.Models
+{
+    public class MRegisterCamera
+    {
+        public string Id { get; set; }
+
+        public string Longitude { get; set; }
+
+        public string Latitude { get; set; }
+    }
+}
