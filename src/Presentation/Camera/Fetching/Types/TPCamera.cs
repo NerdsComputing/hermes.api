054f33466a6824a7e308dbca822a673aea58320a
Constantinescu Andrei <33575385+ConstantinescuAndrei@users.noreply.github.com>
2021-07-26 13:17:22 +0300

    [HER-87] Create camera graph parameter (#48)
    * [HER-87]Create TPCamera
    
    * [HER-87]Follow-up Rename directory
    
    * [HER-87]Follow-up Rename namespace

---
+++ src/Presentation/Camera/Fetching/Types/TPCamera.cs
@@ -0,0 +1,16 @@
+namespace Presentation.Camera.Fetching.Types
+{
+    using System.Diagnostics.CodeAnalysis;
+    using Business.Camera.Fetching.Models;
+    using global::GraphQL.Types;
+    using Style;
+
+    [SuppressMessage(Category.Default, Check.CA1724, Justification = Reason.Readability)]
+    public class TPCamera : InputObjectGraphType<PCamera>
+    {
+        public TPCamera()
+        {
+            Field(camera => camera.Ids).Description("This is the list of ids");
+        }
+    }
+}
