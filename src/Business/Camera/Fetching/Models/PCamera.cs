b02ba4b945353747005ebef23cff3f3350cb8435
Codrut <constantinescu321@gmail.com>
2021-07-26 11:41:51 +0300

    [HER-86]Follow-up Add Pagination and not-nullable

--- src/Business/Camera/Fetching/Models/PCamera.cs
+++ src/Business/Camera/Fetching/Models/PCamera.cs
@@ -1,9 +1,14 @@
 namespace Business.Camera.Fetching.Models
 {
     using System.Collections.Generic;
+    using Business.Pagination;
+
+    #nullable enable
 
     public class PCamera
     {
-        public List<string> Ids { get; set; } = new List<string>();
+        public List<string> Ids { get; set; }
+
+        public PPagination Pagination { get; set; } = new PPagination();
     }
 }
