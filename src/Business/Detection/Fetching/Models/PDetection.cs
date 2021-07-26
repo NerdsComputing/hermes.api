78a876d30b3ddb24d5bbeaceda5811ff685d95e3
Constantinescu Andrei <33575385+ConstantinescuAndrei@users.noreply.github.com>
2021-07-26 10:01:47 +0300

    [HER-76] Create detection parameter (#38)
    * [HER-76]Create PDetection model
    
    * [HER-76]Follow up Initialize Pagination
    
    * [HER-76]Follow-up Move PDetection

---
+++ src/Business/Detection/Fetching/Models/PDetection.cs
@@ -0,0 +1,18 @@
+namespace Business.Detection.Fetching.Models
+{
+    using System;
+    using Business.Pagination;
+
+    public class PDetection
+    {
+        public int Id { get; set; }
+
+        public int Score { get; set; }
+
+        public string Class { get; set; }
+
+        public DateTime Timestamp { get; set; }
+
+        public PPagination Pagination { get; set; } = new PPagination();
+    }
+}
