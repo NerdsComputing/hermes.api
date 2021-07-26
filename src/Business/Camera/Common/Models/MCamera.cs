7f78b65be04236691cddd222490330134f169a43
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-07-26 10:17:57 +0300

    [HER-80]Follow-up Change from int to string

--- src/Business/Camera/Common/Models/MCamera.cs
+++ src/Business/Camera/Common/Models/MCamera.cs
@@ -2,7 +2,7 @@ namespace Business.Camera.Common.Models
 {
     public class MCamera
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
 
         public string Longitude { get; set; }
 
