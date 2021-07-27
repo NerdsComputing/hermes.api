537721e2136632a742bb4ccf0e01bec832d3b886
Urzica-Platon-Casian <casian.platon@ulbsibiu.ro>
2021-07-27 13:50:18 +0300

    [HER-89]Follow-up Check not null and move above

--- src/Data/Camera/Filtering/CameraFilter.cs
+++ src/Data/Camera/Filtering/CameraFilter.cs
@@ -7,13 +7,13 @@ namespace Data.Camera.Filtering
     {
         private PCamera _parameter;
 
-        public IQueryable<ECamera> Execute(IQueryable<ECamera> input) =>
-            input.Where(entity => _parameter.Ids.Contains(entity.Id));
-
         public ICameraFilter With(PCamera parameter)
         {
             _parameter = parameter;
             return this;
         }
+
+        public IQueryable<ECamera> Execute(IQueryable<ECamera> input) =>
+            input.Where(entity => _parameter.Ids == null || _parameter.Ids.Contains(entity.Id));
     }
 }
