4e3371d45fd7b5f7e1e964e5ea3fffb9df659dde
Codrut <constantinescu321@gmail.com>
2021-07-27 17:07:20 +0300

    [Her-96]Add camera field to detections and filters

--- src/Data/Detection/DetectionRepository.cs
+++ src/Data/Detection/DetectionRepository.cs
@@ -6,16 +6,30 @@ namespace Data.Detection
     using Business.Detection.Common.Models;
     using Business.Detection.Common.Repositories;
     using Business.Detection.Creating.Models;
+    using Business.Detection.Fetching.Models;
+    using Data.Detection.Filtering;
+    using Microsoft.EntityFrameworkCore;
 
     public class DetectionRepository : IDetectionRepository
     {
         private readonly Context _context;
+        private readonly IDetectionFilter _filter;
 
-        public DetectionRepository(Context context) => _context = context;
+        public DetectionRepository(Context context, IDetectionFilter filter)
+        {
+            _context = context;
+            _filter = filter;
+        }
 
-        public IEnumerable<MDetection> ByParameter() => _context.Set<EDetection>()
-            .ToList()
-            .Select(DetectionFactory.MakeModel);
+        public IEnumerable<MDetection> ByParameter(PDetection parameter)
+        {
+            var detections = _context.Set<EDetection>()
+                .Include(entity => entity.Camera);
+
+            return _filter.With(parameter).Execute(detections)
+                .ToList()
+                .Select(DetectionFactory.MakeModel);
+        }
 
         public IEnumerable<MDetection> Insert(IEnumerable<MCreateDetection> input) => input
             .Select(DetectionFactory.MakeEntity)
