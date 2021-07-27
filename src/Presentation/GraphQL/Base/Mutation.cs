8fb5540198c987afc9966acb28bba1e5c30601b9
bianca <bia.topircean@gmail.com>
2021-07-27 17:22:41 +0300

    [HER-85] Change mutation

--- src/Presentation/GraphQL/Base/Mutation.cs
+++ src/Presentation/GraphQL/Base/Mutation.cs
@@ -2,20 +2,16 @@ namespace Presentation.GraphQL.Base
 {
     using System;
     using global::GraphQL.Types;
-    using Presentation.Camera.Common.Types;
-    using Presentation.Camera.Register.Types;
-    using Presentation.Detection.Common.Types;
-    using Presentation.Detection.Creating.Types;
 
-    public class Mutation : ObjectGraphType
+    public sealed class Mutation : ObjectGraphType
     {
         public Mutation(IServiceProvider provider)
         {
             Name = "mutation";
             Description = "All the mutations that can be done.";
 
-            MutationFactory<TCreateDetection, TDetection>.Make(ParameterFactory.MakeDetection(this, provider));
-            MutationFactory<TRegisterCamera, TCamera>.Make(ParameterFactory.MakeCamera(this, provider));
+            AddField(Detection.Creating.Factory.Make(provider));
+            AddField(Camera.Register.Factory.Make(provider));
         }
     }
 }
