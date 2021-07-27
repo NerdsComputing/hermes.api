211734ced634bc2cf3395ee4e18321eb9d3cddc8
bianca <bia.topircean@gmail.com>
2021-07-27 14:11:02 +0300

    [HER-85] Create field for register camera in mutation

--- src/Presentation/GraphQL/Base/Mutation.cs
+++ src/Presentation/GraphQL/Base/Mutation.cs
@@ -3,6 +3,8 @@ namespace Presentation.GraphQL.Base
     using System;
     using global::GraphQL.Types;
     using Microsoft.Extensions.DependencyInjection;
+    using Presentation.Camera.Common.Types;
+    using Presentation.Camera.Register.Types;
     using Presentation.Detection.Common.Types;
     using Presentation.Detection.Creating.Types;
 
@@ -11,16 +13,29 @@ namespace Presentation.GraphQL.Base
         public Mutation(IServiceProvider provider)
         {
             Name = "mutation";
+            Description = "All the mutations that can be done.";
 
             Field(typeof(NonNullGraphType<ListGraphType<NonNullGraphType<TDetection>>>),
                 "createDetection",
                 "Creates a detection",
                 arguments: CreateDetectionArguments(),
                 resolve: input => provider.GetService<Detection.Creating.IResolver>().Execute(input));
+
+            Field(typeof(NonNullGraphType<ListGraphType<NonNullGraphType<TCamera>>>),
+                "registerCamera",
+                "Register a camera",
+                arguments: RegisterCameraArguments(),
+                resolve: input => provider.GetService<Camera.Register.IResolver>().Execute(input));
         }
 
         private static QueryArguments CreateDetectionArguments() => new (new QueryArgument(
             typeof(NonNullGraphType<ListGraphType<NonNullGraphType<TCreateDetection>>>))
+        {
+            Name = "input",
+        });
+
+        private static QueryArguments RegisterCameraArguments() => new (new QueryArgument(
+            typeof(NonNullGraphType<ListGraphType<NonNullGraphType<TRegisterCamera>>>))
         {
             Name = "input",
         });
