62549a59a53e90763c9385bacb80edfc1ebe02a5
bianca <bia.topircean@gmail.com>
2021-07-28 14:44:33 +0300

    [HER-88] Use factory for adding field

--- src/Presentation/GraphQL/Base/Query.cs
+++ src/Presentation/GraphQL/Base/Query.cs
@@ -2,14 +2,16 @@ namespace Presentation.GraphQL.Base
 {
     using System;
     using System.Diagnostics.CodeAnalysis;
+    using Business.Camera.Common.Models;
+    using Business.Camera.Fetching.Models;
     using global::GraphQL.Types;
     using Microsoft.Extensions.DependencyInjection;
     using Presentation.Camera.Common.Types;
+    using Presentation.Camera.Fetching.Types;
     using Presentation.Detection.Common.Types;
     using Style;
 
     [SuppressMessage(Category.Default, Check.CA1724, Justification = Reason.Readability)]
-
     public class Query : ObjectGraphType
     {
         public Query(IServiceProvider provider)
@@ -22,10 +24,7 @@ namespace Presentation.GraphQL.Base
                 "Fetch all the existing detections",
                 resolve: input => provider.GetService<Detection.Fetching.IResolver>().Execute(input));
 
-            Field(typeof(NonNullGraphType<ListGraphType<NonNullGraphType<TCamera>>>),
-                "cameras",
-                "Fetch all the existing cameras",
-                resolve: input => provider.GetService<Camera.Fetching.IResolver>().Execute(input));
+            AddField(Presentation.Camera.Fetching.Factory.Make(provider));
         }
     }
 }
