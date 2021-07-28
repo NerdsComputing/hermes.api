8084d21d108d14598a1a680f139ed9d41de88579
Codrut <constantinescu321@gmail.com>
2021-07-28 12:42:19 +0300

    [HER-100]Follow-up Remove sealed class

--- src/Presentation/Pagination/Types/TPPagination.cs
+++ src/Presentation/Pagination/Types/TPPagination.cs
@@ -6,15 +6,15 @@ namespace Presentation.Pagination.Types
     using Style;
 
     [SuppressMessage(Category.Default, Check.CA1724, Justification = Reason.Readability)]
-    public sealed class TPPagination : InputObjectGraphType<PPagination>
+    public class TPPagination : InputObjectGraphType<PPagination>
     {
         public TPPagination()
         {
             Name = "Pagination";
             Description = "It will be used for fetching data for a single page";
 
-            Field(pagination => pagination.PageSize).Description("This is the page size (start at 1)");
-            Field(pagination => pagination.PageIndex).Description("This is the page index (start at 0");
+            Field(pagination => pagination.PageSize).Description("This is the page size (starts at 1)");
+            Field(pagination => pagination.PageIndex).Description("This is the page index (starts at 0");
         }
     }
 }
