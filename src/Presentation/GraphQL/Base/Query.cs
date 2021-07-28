fcc96911807aa10d8ae91fe61c84b2de4d545def
andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-07-28 16:44:39 +0300

    [HER-101]Change the Query for fetching a detection

--- src/Presentation/GraphQL/Base/Query.cs
+++ src/Presentation/GraphQL/Base/Query.cs
@@ -2,13 +2,7 @@ namespace Presentation.GraphQL.Base
 {
     using System;
     using System.Diagnostics.CodeAnalysis;
-    using Business.Camera.Common.Models;
-    using Business.Camera.Fetching.Models;
     using global::GraphQL.Types;
-    using Microsoft.Extensions.DependencyInjection;
-    using Presentation.Camera.Common.Types;
-    using Presentation.Camera.Fetching.Types;
-    using Presentation.Detection.Common.Types;
     using Style;
 
     [SuppressMessage(Category.Default, Check.CA1724, Justification = Reason.Readability)]
@@ -19,11 +13,7 @@ namespace Presentation.GraphQL.Base
             Name = "query";
             Description = "All the queries that can be done.";
 
-            Field(typeof(NonNullGraphType<ListGraphType<NonNullGraphType<TDetection>>>),
-                "detections",
-                "Fetch all the existing detections",
-                resolve: input => provider.GetService<Detection.Fetching.IResolver>().Execute(input));
-
+            AddField(Presentation.Detection.Fetching.Factory.Make(provider));
             AddField(Presentation.Camera.Fetching.Factory.Make(provider));
         }
     }
