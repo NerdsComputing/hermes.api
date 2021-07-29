a0ad8df0f05fe6b964ea044e76d21a0454857f16
Codrut <constantinescu321@gmail.com>
2021-07-29 18:53:28 +0300

    [HER-110]Add TimestampFilter field

--- src/Presentation/Detection/Fetching/Types/TPDetection.cs
+++ src/Presentation/Detection/Fetching/Types/TPDetection.cs
@@ -16,8 +16,9 @@ namespace Presentation.Detection.Fetching.Types
 
             Field(detection => detection.Id, true).Description("This is the ID");
             Field(detection => detection.Score, true).Description("This is the Score");
-            Field(detection => detection.Timestamp, true).Description("This is the Timestamp");
             Field(detection => detection.Class, true).Description("This is the Class");
+            Field(detection => detection.TimestampFilter, true, typeof(TTimestampFilter))
+                .Description("This is the timestamp");
             Field(detection => detection.Pagination, false, typeof(TPPagination)).Description("This is the pagination");
             Field(detection => detection.CameraId, true).Description("The camera id that took the detection");
         }
