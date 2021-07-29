38afe47a53f56ff28e3fe5f6c9102410dd10f464
Codrut <constantinescu321@gmail.com>
2021-07-29 18:52:48 +0300

    [HER-110]Create TTimestampFilter

---
+++ src/Presentation/Detection/Fetching/Types/TTimestampFilter.cs
@@ -0,0 +1,20 @@
+namespace Presentation.Detection.Fetching.Types
+{
+    using System.Diagnostics.CodeAnalysis;
+    using Business.Detection.Fetching.Models;
+    using global::GraphQL.Types;
+    using Style;
+
+    [SuppressMessage(Category.Default, Check.CA1724, Justification = Reason.Readability)]
+    public class TTimestampFilter : InputObjectGraphType<MTimestampFilter>
+    {
+        public TTimestampFilter()
+        {
+            Name = "TimestampFilter";
+            Description = "This will be used for filtering the detections by timestamp";
+
+            Field(timestamp => timestamp.LesserThan, true).Description("Detections will have the timestamp lesser than this value");
+            Field(timestamp => timestamp.GreaterThan, true).Description("Detections will have the timestamp greater than this value");
+        }
+    }
+}
