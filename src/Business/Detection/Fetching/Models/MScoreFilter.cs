b5ec46277b874aca44ef54bc7fdde7297d657e3f
bianca <bia.topircean@gmail.com>
2021-07-30 11:35:07 +0300

    [HER-111] Create MScoreFilter

---
+++ src/Business/Detection/Fetching/Models/MScoreFilter.cs
@@ -0,0 +1,9 @@
+namespace Business.Detection.Fetching.Models
+{
+    public class MScoreFilter
+    {
+        public float? LesserEqualThan { get; set; }
+
+        public float? GreaterEqualThan { get; set; }
+    }
+}
