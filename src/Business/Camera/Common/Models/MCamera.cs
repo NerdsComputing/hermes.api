72cbb961b9235752be6a130ecf4c401b148e2c73
andreea <andreea.dumitrascu@ulbsibiu.ro>
2021-08-03 15:45:08 +0300

    [HER-130]Change from string to double in models

--- src/Business/Camera/Common/Models/MCamera.cs
+++ src/Business/Camera/Common/Models/MCamera.cs
@@ -4,8 +4,8 @@ namespace Business.Camera.Common.Models
     {
         public string Id { get; set; }
 
-        public string Longitude { get; set; }
+        public double Longitude { get; set; }
 
-        public string Latitude { get; set; }
+        public double Latitude { get; set; }
     }
 }
